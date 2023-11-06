14582f454de4494e6c4b80eed29c0e8901abe614
SM RIYAD <smriyad0185@gmail.com>
2023-11-06 18:15:55 +0600

    all job table is done

--- src/pages/Alljobs/AlljobBanner.jsx
+++ src/pages/Alljobs/AlljobBanner.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const AlljobBanner = () => {
+
+    
     return (
         <div className='pb-5'>
         <div className="hero min-h-[350px] bg-cover" style={{backgroundImage: 'url(https://img.freepik.com/free-photo/human-resources-concept-with-people_23-2150389123.jpg?size=626&ext=jpg&ga=GA1.1.2010618524.1694701074&semt=sph)'}}>
 <div className="hero-overlay bg-opacity-80"></div>
 <div className="hero-content text-center text-neutral-content">
 <div className="max-w-md">
-  <h1 className="mb-5 text-5xl  font-bold">ADD  JOBS</h1>
+  <h1 className="mb-5 text-5xl  font-bold">ALL  JOBS</h1>
  
 </div>
 </div>
--- src/pages/Alljobs/Alljobs.jsx
+++ src/pages/Alljobs/Alljobs.jsx
@@ -1,11 +1,82 @@
-import React from 'react';
+import React from "react";
+import AlljobBanner from "./AlljobBanner";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useEffect, useState } from "react";
 
+import "./alljobcss/job.css";
 const Alljobs = () => {
-    return (
-        <div>
-            this is all jobs
+  const axiosSecure = useAxiosSecure();
+  const [jobs, setJobs] = useState([]);
+  const [showjobs, setShowJobs] = useState([]);
+
+  useEffect(() => {
+    axiosSecure.get("alljobs").then((result) => {
+      setJobs(result.data);
+      setShowJobs(result.data);
+    });
+  }, [axiosSecure]);
+
+  console.log("this is job array", jobs);
+
+  const {_id,
+    jobPhoto,
+    JobTitle,
+    UserName,
+    useremail,
+    JobCategory,
+    SalaryRange,
+    JobDescription,ApplicationDeadline,JobPostingDate,JobApplicantsNumber
+  } = showjobs;
+  return (
+    <div>
+      <AlljobBanner></AlljobBanner>
+
+      <div className="mx-auto max-w-6xl">
+        <div className="lg:block hidden">
+          <div className="overflow-x-auto">
+            <table className="table">
+        
+              <thead>
+                <tr>
+                  <th>Name Who posted</th>
+                  <th>Job Title</th>
+                  <th>Job Posting Date</th>
+                  <th>Application Deadline </th>
+                  <th>Salary Range</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+             
+                {
+showjobs.map((job,idx)=>  <tr key={idx}  >   <td>{job?.UserName}</td>
+<td>{job?.JobTitle}</td>
+<td>{job.JobPostingDate}</td>
+<td>{job.ApplicationDeadline}</td>
+<td>${job.SalaryRange}</td>
+<th>
+  <button className="btn bg-green-600 text-white btn-xs">details</button>
+</th>
+
+
+
+
+</tr>)
+
+
+                }
+
+
+
+
+              </tbody>
+            
+            </table>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Alljobs;
